031699ecb6a6ca2a640303d5eca469f4386758ae
srisriraghu1967 <63004377+srisriraghu1967@users.noreply.github.com>
2020-04-01 20:55:25 +0530

    commited this first time

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
